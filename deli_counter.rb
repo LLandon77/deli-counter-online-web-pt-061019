d358dded9478e5ed68a17d4032487750dbf99471
Lura Landon <lura.landon@gmail.com>
2019-07-09 21:04:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def take_a_number(katz_deli, name)
+  
+end
+def line(deli_line)
+    if deli_line.length == 0 
+    puts "The line is currently empty."
+    else 
+      
+end
